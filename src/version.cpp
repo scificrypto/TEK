db58dc5f1b133128e05fb77c0941249c49350391
scificrypto <realgrandnagus@gmail.com>
2019-03-23 20:46:18 -0400

    Update version

--- src/version.cpp
+++ src/version.cpp
@@ -36,8 +36,8 @@ const std::string CLIENT_NAME("TEKcoin");
 // git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 // #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#    define GIT_COMMIT_ID "f96a5a"
-#    define GIT_COMMIT_DATE "$Format:%cD"
+#    define GIT_COMMIT_ID ""
+#    define GIT_COMMIT_DATE "23 Mar 2019"
 #endif
 
 #define BUILD_DESC_FROM_COMMIT(maj,min,rev,build,commit) \
